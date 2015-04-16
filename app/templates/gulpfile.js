3f7885e05c00fca8d8ac43a25f3f312388feb12c
maersu <me@maersu.ch>
2015-04-15 19:45:58 -0700

    use encoded package

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -8,7 +8,7 @@ var reload = browserSync.reload;
 var htmlhint = require('gulp-htmlhint');
 var rev = require('gulp-rev');
 var revReplace = require('gulp-rev-replace');
-var base64Replace = require('gulp-base64-href-replace');
+var encodeReplace = require('gulp-encode-href-replace');
 
 gulp.task('styles', function () {
     return gulp.src('app/styles/main.scss')
@@ -147,15 +147,15 @@ gulp.task('revreplace', ['revision'], function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('base64Replace', [], function () {
+gulp.task('encodeReplace', [], function () {
     return gulp.src('dist/*.html')
-        .pipe(base64Replace())
+        .pipe(encodeReplace())
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['validate', 'html', 'images', 'fonts', 'extras'], function () {
     gulp.start('revreplace');
-    gulp.start('base64Replace');
+    gulp.start('encodeReplace');
     return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
